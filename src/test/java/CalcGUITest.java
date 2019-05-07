1233de952e9522c3216369d2a5f2e3da5c731dbc
jenkins <joseph.enright@students.ittralee.ie>
2019-05-07 18:33:14 +0100

    added tests

---
+++ src/test/java/CalcGUITest.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author joeenright
+ */
+public class CalcGUITest {
+    
+    public CalcGUITest() {
+    }
+    
+    @BeforeAll
+    public static void setUpClass() {
+    }
+    
+    @AfterAll
+    public static void tearDownClass() {
+    }
+
+    @Test
+    public void testSomeMethod() {
+       
+    }
+    
+}
